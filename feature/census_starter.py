1a1a35ffa9697c45c64bac26e88b5fb410ff3aa7
Apisit Junkun <apisit.jn@gmail.com>
2023-02-20 20:43:45 +0700

    change cln census

--- feature/census_starter.py
+++ feature/census_starter.py
@@ -2,8 +2,7 @@ import pandas as pd
 from configs import CENSUS_STARTER_PATH
 
 
-def get_census_starter_feats() -> pd.DataFrame:
-    census_df = pd.read_csv(CENSUS_STARTER_PATH)
+def cln_census_starter_df(census_df: pd.DataFrame) -> pd.DataFrame:
     all_cols = list(census_df.columns)
     feature_cols = [col for col in all_cols if col != 'cfips']
     feature_nms = list(set([col[:-5] for col in feature_cols]))
@@ -14,15 +13,12 @@ def get_census_starter_feats() -> pd.DataFrame:
     census_features = pd.pivot_table(tmp_df, index=['cfips', 'year'], columns='feature', values='value').reset_index()
 
     # populate 12 months
-    month_df = pd.DataFrame({'month': [f'{str(i).zfill(2)}-01' for i in range(1, 13)]})
+    # month_df = pd.DataFrame({'month': [f'{str(i).zfill(2)}-01' for i in range(1, 13)]})
 
     # cross join
-    census_features['tmp_key'] = 0
-    month_df['tmp_key'] = 0
+    # census_features['tmp_key'] = 0
+    # month_df['tmp_key'] = 0
 
-    census_features = census_features.merge(month_df, on='tmp_key', how='outer')
-    census_features = census_features.drop(columns='tmp_key')
-    census_features['row_id'] = census_features['cfips'].astype(str) + '_' + \
-                                census_features['year'] + '-' + census_features['month']
-    census_features = census_features[['row_id'] + feature_nms]
+    # census_features = census_features.merge(month_df, on='tmp_key', how='outer')
+    # census_features = census_features.drop(columns='tmp_key')
     return census_features
--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import pandas as pd
 
 from configs import TRAIN_PATH, CENSUS_STARTER_PATH, OUTPUT_PATH, TEST_PATH, n_period
 from helper_functions import get_t_minus_n_period
-from feature.census_starter import get_census_starter_feats
+from feature.census_starter import cln_census_starter_df
 
 
 pd.set_option('display.max_columns', 20)
 train_df = pd.read_csv(TRAIN_PATH, parse_dates=['first_day_of_month'])
-census_df = get_census_starter_feats()
+census_df = pd.read_csv(CENSUS_STARTER_PATH)
 test_df = pd.read_csv(TEST_PATH)
 
 train_df.rename(columns={'first_day_of_month': 't'}, inplace=True)
 train_df = get_t_minus_n_period(df=train_df, column_nm='t', n_period=n_period)
+census_df = cln_census_starter_df(census_df)
 
 print(census_df.head())
